54ef61302e0453f0a7594c386c7f862aeb1dd78f
leninepestana <leninepestana@gmail.com>
2021-09-16 09:27:14 +0100

    Searching in DB for a specific entry

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -11,21 +11,33 @@ public class Program {
 	public static void main(String[] args) {
 		
 		// Adding data to database
+		/*
 		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
 		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
 		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-	
+		*/
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		
+
+		/*
 		em.getTransaction().begin();
-		
+
 		em.persist(p1);
 		em.persist(p2);
 		em.persist(p3);
 		
 		em.getTransaction().commit();
+		*/
+		
+		// Searching database
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
 		
 		System.out.println("Pronto!");
+		
+		em.close();
+		emf.close();
 	}
 }
